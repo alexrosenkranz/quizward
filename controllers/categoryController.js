fb096459ced631babb87e762f35bdf8c189e57dd
alex@netwaveinteractive.com <alex.rosenkranz@gmail.com>
2017-03-09 21:42:12 -0500

    working with multer

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var Models = require('../Models');
-
+var multer = require('multer');
 var router = express.Router();
+// router.use(multer({ dest: './public/uploads/' }));
 
 // =========== GET ROUTES ===========
 // TEST ROUTE --- comment out later
@@ -20,8 +21,10 @@ router.get('/new', function(req, res) {
 
 // =========== POST ROUTES ===========
 // Create User
-router.post('/create', (req, res) => {
-
+router.post('/new', (req, res) => {
+  console.log(req.body); //form fields
+  console.log(req.file);
+  res.redirect('/categories/new');
 });
 
 
