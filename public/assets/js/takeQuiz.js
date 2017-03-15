82d256ceb4fedf2235d90194aa442a403a0bd179
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2017-03-15 15:29:09 -0400

    query quizzes to be taken and beginning submission

---
+++ public/assets/js/takeQuiz.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+  getQuiz();
+
+  function getQuiz() {
+    var quizId = parseInt($('form').attr('quiz-id'));
+    $.get('/api/quiz/' + quizId + '/questions', function(quiz) {
+      printQuiz(quiz);
+
+    });
+  }
+
+  function printQuiz(q) {
+    var questionContainer = $('.questions-container');
+    for (var i = 0; i < q.Questions.length; i++) {
+
+      var singleQuestion = $('<div>').addClass('form-group').attr('question-number', i);
+
+      var questionTitle = $('<label>').attr('for', i).html(q.Questions[i].question);
+      singleQuestion.append(questionTitle);
+      var choices = JSON.parse(q.Questions[i].choice);
+
+      for (var k = 0; k < choices.length; k++) {
+        var radioGroup = $('<div>').addClass('radio');
+        var radioLabel = $('<label>');
+        var input = $('<input>').attr('type', 'radio').attr('name', 'optionsRadios' + i).attr('id', k).val(choices[k]);
+        radioLabel.append(input).append(choices[k]);
+        radioGroup.append(radioLabel);
+        singleQuestion.append(radioGroup);
+      }
+
+      questionContainer.append(singleQuestion);
+    }
+  }
+
+  $('.take-quiz').on('click', 'button', function(e) {
+    e.preventDefault();
+    var choicesPicked = [];
+    $('.take-quiz').find('.form-group').each(function() {
+      choicesPicked.push($(this).find('input[type=radio]:checked').val());
+    });
+    console.log(choicesPicked);
+  });
+
+});
